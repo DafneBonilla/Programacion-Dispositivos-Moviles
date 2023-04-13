baf9a9261458b2184755cb326d39f45aebddcf0f
Erick Arroyo <Erickarroyo18>
2023-04-12 21:52:52 -0600

    [feat] Primer actividad

---
+++ Tareas/Tarea-02/Curso/app/src/main/java/com/example/curso/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.curso;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
