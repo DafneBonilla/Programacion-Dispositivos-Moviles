4154450e1c0028e52f491dad9ff20ea32e91b504
DafneBonilla <daphnebonilla09@gmail.com>
2023-04-13 21:07:09 -0600

    Tarea 02: Vista login lista y vista menú lista

--- Tareas/Tarea-02/Curso/app/src/main/java/com/example/curso/Confirmacion.java
+++ Tareas/Tarea-02/Curso/app/src/main/java/com/example/curso/Confirmacion.java
@@ -1,19 +1,13 @@
 package com.example.curso;
 
-import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+public class Confirmacion extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_orden);
+        setContentView(R.layout.activity_confirmacion);
     }
-
-
 }
